c6987da7057c268f699bfefd0e6b3a53d16466ae
soran011 <haruti.soran@gmail.com>
2021-01-13 00:59:24 -0500

    fetching data from API

---
+++ src/API.js
@@ -0,0 +1,5 @@
+const API = {
+    users: 'https://api.npoint.io/b9664298da2e4b46a71f'
+}
+
+export default API;
--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import Posts from './components/Posts';
+import Users from './components/Users';
 import Pagination from './components/Pagination';
+import API from './API';
 import axios from 'axios';
 import './App.css';
 
 const App = () => {
-  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(20);
+  const [usersPerPage] = useState(20);
+
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchUsers = async () => {
       setLoading(true);
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      setPosts(res.data);
+      const res = await axios.get(API.users);
+      setUsers(res.data);
       setLoading(false);
     };
 
-    fetchPosts();
+    fetchUsers();
   }, []);
 
   // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
 
   // Change page
   const paginate = pageNumber => setCurrentPage(pageNumber);
@@ -32,10 +34,10 @@ const App = () => {
   return (
     <div className='container mt-5'>
       <h1 className='text-primary mb-3'>Users</h1>
-      <Posts posts={currentPosts} loading={loading} />
+      <Users users={currentUsers} loading={loading} />
       <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
+        usersPerPage={usersPerPage}
+        totalUsers={users.length}
         paginate={paginate}
       />
     </div>
