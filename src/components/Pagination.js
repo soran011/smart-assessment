fcb956d31ba5298aed68fa9da352093d292c34ef
soran011 <haruti.soran@gmail.com>
2021-01-13 00:57:35 -0500

    finalizing pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+const Pagination = ({ usersPerPage, totalUsers, paginate }) => {
     const pageNumbers = [];
 
-    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    for (let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i++) {
         pageNumbers.push(i);
     }
 
